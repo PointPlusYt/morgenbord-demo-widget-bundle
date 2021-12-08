c7c12dd59f75ff98976da770907cc9644b5acea5
Djyp <d@tbnk.ca>
2021-12-09 00:04:54 +0100

    Testing new extension

---
+++ src/DependencyInjection/MorgenBordDemoWidgetExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MorgenBord\DemoWidgetBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class MorgenBordDemoWidgetExtension extends Extension implements PrependExtensionInterface
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../config')
+        );
+        $loader->load('services.yaml');
+        $loader->load('routes/demo_widget.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        // ...
+    }
+}
