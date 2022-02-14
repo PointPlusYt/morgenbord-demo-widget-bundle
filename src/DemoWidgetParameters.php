4ca84a059383ac82f4d9e0435882d5a377fb993d
Djyp <d@tbnk.ca>
2022-02-14 09:58:02 +0100

    Just making a better form

--- src/DemoWidgetParameters.php
+++ src/DemoWidgetParameters.php
@@ -4,6 +4,9 @@ namespace MorgenBord\DemoWidgetBundle;
 
 use App\Interfaces\ParametersFormTypeInterface;
 use App\Widget\ParametersForms;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class DemoWidgetParameters implements ParametersFormTypeInterface
@@ -11,8 +14,18 @@ class DemoWidgetParameters implements ParametersFormTypeInterface
     public function createParametersForm(FormBuilderInterface $builder)
     {
         $builder
-            ->add('text')
-            ->add('number')
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'required' => false,
+            ])
+            ->add('number', IntegerType::class, [
+                'label' => 'Nombre',
+                'required' => false,
+            ])
+            ->add('longtext', TextareaType::class, [
+                'label' => 'Long texte',
+                'required' => false,
+            ])
         ;
     }
 }
