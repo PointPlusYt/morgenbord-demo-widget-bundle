d0c8c324250f9302bd3e16ce7cfd573eb4091dab
Djyp <d@tbnk.ca>
2022-02-15 10:23:25 +0100

    Changed the architecture to only one file per widget

--- src/DemoWidgetParameters.php
+++ src/DemoWidgetParameters.php
@@ -2,6 +2,8 @@
 
 namespace MorgenBord\DemoWidgetBundle;
 
+use App\Entity\Widget;
+use App\Event\RegisterWidgetEvent;
 use App\Interfaces\ParametersFormTypeInterface;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -10,6 +12,21 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class DemoWidgetParameters implements ParametersFormTypeInterface
 {
+    public const WIDGET_NAME = 'Demo Widget';
+    public const WIDGET_SHORT_NAME = 'demo_widget';
+    public const TWIG_FILE = '@DemoWidget/widget.html.twig';
+
+    public function onMorningBordRegisterWidget(RegisterWidgetEvent $event)
+    {
+        $widget = new Widget();
+        $widget->setName(self::WIDGET_NAME);
+        $widget->setShortName(self::WIDGET_SHORT_NAME);
+        $widget->setTwigFile(self::TWIG_FILE);
+        $widget->setParameterFormFqcn(DemoWidgetBundle::class);
+        
+        $event->addWidget($widget);
+    }
+    
     public function createParametersForm(FormBuilderInterface $builder)
     {
         $builder
